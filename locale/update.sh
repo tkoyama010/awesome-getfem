07079f445111b612ae62debf83085f18cde45665
Tetsuo Koyama <tkoyama010@gmail.com>
2020-05-17 14:08:07 +0900

    :shirt: doc (#31)
    * :shirt: doc
    
    * :shirt: doc
    
    * :shirt: doc
    
    * :shirt: doc

--- locale/update.sh
+++ locale/update.sh
@@ -6,7 +6,7 @@ LOCAL_PYTHON_PATH="/home/runner/.local/bin"
 # pull po files from transifex
 cd `dirname $0`
 $LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../ pot
+$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../doc pot
 $LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 $LOCAL_PYTHON_PATH/tx push -s --skip
