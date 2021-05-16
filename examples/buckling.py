c9e8d619d43f8dcf0059bb328d657bc0f1b6a59c
Tetsuo Koyama <tkoyama010@gmail.com>
2021-05-16 19:01:40 +0900

    Add examples/buckling.py

---
+++ examples/buckling.py
@@ -0,0 +1,112 @@
+import getfem as gf
+import numpy as np
+import pyvista as pv
+
+pv.set_plot_theme("document")
+
+# Yong modulus (MPa)
+E = 10000.0
+# Poisson ratio
+nu = 0.0
+# First Lame coefficient (MPa)
+clambda = E * nu / ((1 + nu) * (1 - 2 * nu))
+# Second Lame coefficient (MPa)
+cmu = E / (2 * (1 + nu))
+# Initially imperfect (mm)
+alphas = [0.0, 0.02, 0.2]
+# X length (mm)
+b = 1.0
+# Y length (mm)
+h = 1.0
+# Z length (mm)
+L = 20.0
+# Degree of the finite element methods
+elements_degree = 2
+
+meshs = []
+p = pv.Plotter(shape=(1, len(alphas)))
+for i, alpha in enumerate(alphas):
+
+    x = np.linspace(0.0, b, 4)
+    y = np.linspace(0.0, h, 4)
+    z = np.linspace(0.0, L, 80)
+
+    mesh = gf.Mesh("cartesian", x, y, z)
+    pts = mesh.pts()
+    pts += np.array([0.0*pts[2], alpha*(1.0 - np.cos(np.pi*pts[2]/(L/2.0))), 0.0*pts[2]])
+    mesh.set_pts(pts)
+    meshs.append(mesh)
+    mesh.export_to_vtk("mesh.vtk", "ascii")
+
+    p.subplot(0, i)
+    m = pv.read("mesh.vtk")
+    p.add_mesh(m, show_edges=True)
+    p.camera.zoom(2)
+    p.show_grid()
+
+p.show(screenshot="mesh.png", window_size=[1200, 1400])
+
+for mesh in meshs:
+
+    fb1 = mesh.outer_faces_with_direction([0.0, 0.0, 1.0], 0.01)
+    fb2 = mesh.outer_faces_with_direction([0.0, 0.0, -1.0], 0.01)
+
+    TOP_BOUND = 1
+    BOTTOM_BOUND = 2
+
+    mesh.set_region(TOP_BOUND, fb1)
+    mesh.set_region(BOTTOM_BOUND, fb2)
+
+mfus = []
+mims = []
+for mesh in meshs:
+
+    mfu = gf.MeshFem(mesh, 3)
+    mfu.set_classical_fem(elements_degree)
+    mfus.append(mfu)
+
+    mim = gf.MeshIm(mesh, pow(elements_degree, 2))
+    mims.append(mim)
+
+mds = []
+for mfu, mim in zip(mfus, mims):
+
+    md = gf.Model("real")
+    md.add_fem_variable("u", mfu)
+    mds.append(md)
+
+for md, mfu, mim in zip(mds, mfus, mims):
+
+    md.add_initialized_data("params", [clambda, cmu])
+    lawname = "SaintVenant Kirchhoff"
+    md.add_finite_strain_elasticity_brick(mim, lawname, "u", "params")
+
+for md, mfu, mim in zip(mds, mfus, mims):
+
+    md.add_initialized_data("r1", [0.0, 0.0, -2.0])
+    md.add_initialized_data("r2", [0.0, 0.0, 0.0])
+    md.add_initialized_data("H1", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+    md.add_initialized_data("H2", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+    md.add_generalized_Dirichlet_condition_with_multipliers(mim, "u", mfu, TOP_BOUND, "r1", "H1")
+    md.add_generalized_Dirichlet_condition_with_multipliers(mim, "u", mfu, BOTTOM_BOUND, "r2", "H2")
+
+for md in mds:
+
+    md.solve("max_res", 1E-9, "max_iter", 100, "noisy", "lsearch", "simplest",  "alpha min", 0.8)
+
+
+p = pv.Plotter(shape=(1, len(alphas)))
+for i, (md, mfu) in enumerate(zip(mds, mfus)):
+
+    U = md.variable("u")
+    mfu.export_to_vtk("displacement.vtk", "ascii", mfu, U, "Displacements")
+
+    p.subplot(0, i)
+    d = pv.read("displacement.vtk")
+    d.set_active_vectors("Displacements")
+    p.add_mesh(d.warp_by_vector(factor=1.00), show_edges=True)
+    p.camera_position = "yz"
+    p.camera.zoom(1.5)
+    p.show_grid()
+
+p.show(screenshot="displacement.png", window_size=[1200, 1400])
