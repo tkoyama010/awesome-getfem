7e0cab01d7f1715fd715124d59a298029e6b3895
Tetsuo Koyama <tkoyama010@gmail.com>
2021-06-02 09:30:45 +0900

    Update for loop

--- examples/buckling.py
+++ examples/buckling.py
@@ -22,8 +22,12 @@ h = 1.0
 L = 20.0
 # Degree of the finite element methods
 elements_degree = 2
-# Displacements of the TOP boundary
-displacements = np.linspace(0.0, 2.0, 100)
+# Linear
+linear = False
+# Force
+# forces = np.arange(0.0, 100.0 + 1.0, 1.0)
+# forces = np.array([0.0, 10.0, 20.0, 30.0, 4.0, 50.0, 60.0, 70.0, 80.0, 90.0 , 100.0])
+forces = np.array([0.0, 10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0 , 100.0])
 
 meshs = []
 p = pv.Plotter(shape=(1, len(alphas)))
@@ -39,6 +43,8 @@ for i, alpha in enumerate(alphas):
         [0.0 * pts[2], alpha * (1.0 - np.cos(np.pi * pts[2] / (L / 2.0))), 0.0 * pts[2]]
     )
     mesh.set_pts(pts)
+    # mesh.merge(gf.Mesh("cartesian", x, y, np.array([L, L + 1.0])))
+
     meshs.append(mesh)
     mesh.export_to_vtk("mesh" + str(i) + ".vtk", "ascii")
 
@@ -54,6 +60,10 @@ p.show(screenshot="mesh.png", window_size=[1200, 1400])
 
 for mesh in meshs:
 
+    # P = mesh.pts()
+    # c1 = (P[2, :] > L - 1e-6)
+    # pid1 = np.compress(c1, list(range(0, mesh.nbpts())))
+    # fb1 = mesh.faces_from_pid(pid1)
     fb1 = mesh.outer_faces_with_direction([0.0, 0.0, 1.0], 0.01)
     fb2 = mesh.outer_faces_with_direction([0.0, 0.0, -1.0], 0.01)
 
@@ -83,15 +93,26 @@ for mfu, mim in zip(mfus, mims):
 
 for md, mfu, mim in zip(mds, mfus, mims):
 
-    md.add_initialized_data("params", [clambda, cmu])
-    lawname = "SaintVenant Kirchhoff"
-    md.add_finite_strain_elasticity_brick(mim, lawname, "u", "params")
+    if linear:
+        md.add_initialized_data('cmu1', cmu)
+        md.add_initialized_data('clambda1', clambda)
+        md.add_isotropic_linearized_elasticity_brick(mim, 'u', 'clambda1', 'cmu1')
+        md.add_initialized_data('cmu2', cmu*1000.0)
+        md.add_initialized_data('clambda2', clambda*1000.0)
+        md.add_isotropic_linearized_elasticity_brick(mim, 'u', 'clambda2', 'cmu2', TOP_BOUND)
+    else:
+        md.add_initialized_data("params", [clambda, cmu])
+        md.add_initialized_data("lambda", clambda*1000.0)
+        md.add_initialized_data("mu", cmu*1000.0)
+        lawname = "SaintVenant Kirchhoff"
+        md.add_finite_strain_elasticity_brick(mim, lawname, "u", "params")
+        md.add_isotropic_linearized_elasticity_brick(mim, "u", "lambda", "mu", TOP_BOUND)
 
 for md, mfu, mim in zip(mds, mfus, mims):
 
-    md.add_initialized_data("r1", [0.0, 0.0, -2.0])
+    md.add_initialized_data("r1", [0.0, 0.0, 0.0])
     md.add_initialized_data("r2", [0.0, 0.0, 0.0])
-    md.add_initialized_data("H1", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+    md.add_initialized_data("H1", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 0.0]])
     md.add_initialized_data("H2", [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
     md.add_generalized_Dirichlet_condition_with_multipliers(
         mim, "u", mfu, TOP_BOUND, "r1", "H1"
@@ -100,36 +121,54 @@ for md, mfu, mim in zip(mds, mfus, mims):
         mim, "u", mfu, BOTTOM_BOUND, "r2", "H2"
     )
 
-for i, (md, mfu) in enumerate(zip(mds, mfus)):
+curves = []
+for i, (md, mfu, mim) in enumerate(zip(mds, mfus, mims)):
+    # md.add_initialized_data("force", [0.0, 0.0, 0.0])
 
-    md.solve(
-        "max_res",
-        1e-9,
-        "max_iter",
-        100,
-        "noisy",
-        "lsearch",
-        "simplest",
-        "alpha min",
-        0.8,
-    )
+    Fs = []
+    displacements = []
+    # for j, force in enumerate(forces):
+    for j in range(100):
+        # md.set_variable("force", [0.0, 0.0, -force])
+        md.add_source_term_brick(mim, "u", "[0.0, 0.0, -1.0]", TOP_BOUND)
+        iter_number = md.solve(
+            "max_res",
+            1e-6,
+            "max_iter",
+            200,
+            "noisy",
+            "lsearch",
+            "simplest",
+            "alpha min",
+            0.8,
+        )
 
-    U = md.variable("u")
-    mfu.export_to_vtk(
-        "displacement" + str(i) + ".vtk", "ascii", mfu, U, "Displacements"
-    )
+        if iter_number == 200:
+           break
+        displacement = (((md.variable("u"))[mfu.dof_on_region(TOP_BOUND)]).reshape(-1, 3))[0, 2]
+        if np.abs(displacement) > 20.0:
+          break
+
+        displacements.append(displacement)
+        Fs.append(j*1.0)
+        U = md.variable("u")
+        mfu.export_to_vtk(
+            "displacement" + str(i) + "-" + "{:0=3}".format(j) + ".vtk", "ascii", mfu, U, "Displacements"
+        )
+
+    curves.append([Fs, displacements])
 
 
-p = pv.Plotter(shape=(1, len(alphas)))
-for i, alpha in enumerate(alphas):
-
-    p.subplot(0, i)
-    d = pv.read("displacement" + str(i) + ".vtk")
-    d.set_active_vectors("Displacements")
-    p.add_mesh(d.warp_by_vector(factor=1.00), show_edges=True, color="white")
-    p.camera_position = "yz"
-    p.camera.enable_parallel_projection()
-    p.camera.zoom(1.75)
-    p.show_grid()
-
-p.show(screenshot="displacement.png", window_size=[1200, 1400])
+# p = pv.Plotter(shape=(1, len(alphas)))
+# for i, alpha in enumerate(alphas):
+# 
+#     p.subplot(0, i)
+#     d = pv.read("displacement" + str(i) + ".vtk")
+#     d.set_active_vectors("Displacements")
+#     p.add_mesh(d.warp_by_vector(factor=1.00), show_edges=True, color="white")
+#     p.camera_position = "yz"
+#     p.camera.enable_parallel_projection()
+#     p.camera.zoom(1.75)
+#     p.show_grid()
+# 
+# p.show(screenshot="displacement.png", window_size=[1200, 1400])
